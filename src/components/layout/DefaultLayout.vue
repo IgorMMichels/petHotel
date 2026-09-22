3337b4ceeb0ff7c10f1602b0488fee5341cd0d9c
Igor Michels <igor.marcon.michels@gmail.com>
2026-09-22 09:15:17 -0300

    feat: Iniciação Projeto

---
+++ src/components/layout/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import AppNavbar from './appNavbar.vue';
+import AppSidebar from './appSidebar.vue';
+</script>
+
+<template>
+  <AppNavbar />
+  <div class="container-fluid">
+    <div class="row">
+      <aside class="col-md-3"></aside>
+    </div>
+  </div>
+</template>
---
+++ src/components/layout/appNavbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- As a link -->
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <a
+        class="navbar-brand"
+        href="#"
+        >Navbar</a
+      >
+      <div class="container-fluid">
+        <router-link
+          class="navbar-brand"
+          to="/pets"
+          >Pets</router-link
+        >
+      </div>
+    </div>
+  </nav>
+</template>
